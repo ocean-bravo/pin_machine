741bd785123b61bea8290937da070046ad61f87f
Sergey Strelnikov <sergey.str@gmail.com>
2023-10-07 23:23:25 +0300

    test

--- app/core/my_image_provider.h
+++ app/core/my_image_provider.h
@@ -5,12 +5,9 @@
 #include <QPainter>
 #include <QMutex>
 #include <QQuickImageProvider>
-
-#include "utils.h"
-
 #include <QReadWriteLock>
 
-
+#include "utils.h"
 
 class MyImageProvider : public QObject, public QQuickImageProvider
 {
@@ -21,91 +18,17 @@ public:
 
     Q_INVOKABLE void clearCaptured();
 
-    void setPixmap(const QPixmap& pixmap)
-    {
-        _pixmap = pixmap;
-    }
-
-    void setImage(const QImage& image, const QString& id)
-    {
-        //qd() << "set image " << id;
-        _lock.lockForWrite();
-        _images.insert(id, image);
-        _lock.unlock();
-
-        emit imageChanged(id);
-    }
-
-    QImage requestImage(const QString &id, QSize *size, const QSize &requestedSize) override
-    {
-        //qd() << " request image " << id <<  size << requestedSize;
-
-
-//            int width = 100;
-//            int height = 50;
-
-//            if (size)
-//                *size = QSize(width, height);
-
-//            QPixmap pixmap(requestedSize.width() > 0 ? requestedSize.width() : width,
-//                           requestedSize.height() > 0 ? requestedSize.height() : height);
-//            pixmap.fill(QColor(id).rgba());
-            // write the color name
-//            QPainter painter(&_pixmap);
-//            QFont f = painter.font();
-//            f.setPixelSize(20);
-//            painter.setFont(f);
-//            painter.setPen(Qt::black);
-//            if (requestedSize.isValid())
-//                painter.scale(requestedSize.width() / width, requestedSize.height() / height);
-//            painter.drawText(QRectF(0, 0, width, height), Qt::AlignCenter, id);
-
-
-        //qd() << "request image " << id;
-        _lock.lockForRead();
-        QImage img = _images.value(id);
-        _lock.unlock();
-
-        return img;
-    }
-
-    QPixmap requestPixmap(const QString &id, QSize *size, const QSize &requestedSize) override
-    {
-
-        qd() << " request pixmap " << id <<  size << requestedSize;
-        int width = 100;
-        int height = 50;
-
-        if (size)
-            *size = QSize(width, height);
-        QPixmap pixmap(requestedSize.width() > 0 ? requestedSize.width() : width,
-                       requestedSize.height() > 0 ? requestedSize.height() : height);
-        pixmap.fill(QColor(id).rgba());
-
-        //write the color name
-        QPainter painter(&_pixmap);
-        QFont f = painter.font();
-        f.setPixelSize(20);
-        painter.setFont(f);
-        painter.setPen(Qt::black);
-        if (requestedSize.isValid())
-            painter.scale(requestedSize.width() / width, requestedSize.height() / height);
-        painter.drawText(QRectF(0, 0, width, height), Qt::AlignCenter, id);
-
-        qd() << " request pixmap " << id <<  size << requestedSize;
-
-        return _pixmap;
-    }
+    void setPixmap(const QPixmap& pixmap);
+    void setImage(const QImage& image, const QString& id);
+    QImage requestImage(const QString &id, QSize *size, const QSize &requestedSize) override;
+    QPixmap requestPixmap(const QString &id, QSize *size, const QSize &requestedSize) override;
 
 signals:
     void imageChanged(QString id);
 
-
 private:
     QPixmap _pixmap;
     QImage _image;
-
     QReadWriteLock _lock;
-
     QMap<QString, QImage> _images;
 };
