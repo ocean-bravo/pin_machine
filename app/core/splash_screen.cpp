20a8caea4a1d6d921fe876db6715ba3706cb4512
Sergey Strelnikov <sergey.str@gmail.com>
2023-11-24 18:00:05 +0300

    save_load

--- app/core/splash_screen.cpp
+++
@@ -1,9 +0,0 @@
-#include "splash_screen.h"
-
-#include <QGuiApplication>
-#include <QBackingStore>
-#include <QResizeEvent>
-#include <QPainter>
-#include <QGradient>
-#include <QFont>
-#include <QScreen>
