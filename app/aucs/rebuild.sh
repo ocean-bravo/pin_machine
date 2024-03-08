0c33dcac8d10a0aa8f1210d361bd51212f33c5f0
Sergey Strelnikov <sergey.str@gmail.com>
2024-03-08 22:10:04 +0300

    test2

--- app/aucs/rebuild.sh
+++ app/aucs/rebuild.sh
@@ -9,3 +9,9 @@ cd .. && rm -rf build &&  mkdir build && cd build
 /home/mint/Qt/5.15.2/gcc_64/bin/qmake .. -spec linux-g++ CONFIG+=debug CONFIG+=qml_debug
 
 make -j3 && make install
+
+cd /home/mint/deploy/pin_machine
+
+rm settings.ini
+
+cp settings2.ini settings.ini
