2cd9fa800d1813274c271859b09921ff92f8c137
Sergey Strelnikov <sergey.str@gmail.com>
2024-04-09 01:10:07 +0930

    ok_overlay_graphics_view

--- app/core/mainwindow3.cpp
+++ app/core/mainwindow3.cpp
@@ -7,26 +7,79 @@ MainWindow3::MainWindow3(QWidget *parent)
 {
     ui->setupUi(this);
 
-QString style = R"(
+    //Supports only the background, background-clip and background-origin properties.
 
-    QMainWindow {
-        background-color: rgb(80, 90, 10);
-    }
+    //setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
 
-    centralWidget {
-    background: yellow;
-    width: 10px; /* when vertical /
-    height: 10px; / when horizontal */
-    }
+    // QString style = R"(
 
-    centralWidget:hover {
-    background: red;
-    }
-    )";
+    //     QMainWindow::separator {
+    //         background: yellow;
+    //         width: 10px; /* when vertical */
+    //         height: 10px; /* when horizontal */
+    //     }
 
 
+    //     QDockWidget {
+    //         titlebar-close-icon: url(theme:Dark/close.svg);
+    //         titlebar-normal-icon: url(theme:Dark/popout.svg);
+    //     }
 
-    setStyleSheet(style);
+    //     QDockWidget::title {
+    //         text-align: center;
+    //         background-color: rgb(70,69,70);
+    //     }
+
+    //     QDockWidget::close-button, QDockWidget::float-button {
+    //         border: 1px solid transparent;
+    //         background: transparent;
+    //         padding: 0px;
+    //     }
+
+    //     QDockWidget::close-button:hover, QDockWidget::float-button:hover {
+    //         background: transparent;
+    //     }
+
+    //     QDockWidget::close-button:pressed, QDockWidget::float-button:pressed {
+    //         padding: 1px -1px -1px 1px;
+    //     }
+    //     )";
+
+
+    // QMainWindow {
+    //     background-color: rgb(80, 90, 10);
+    // }
+
+    // QWidget#MainWindow3 {
+    //     background-color: red;
+    // }
+
+
+
+    // QMainWindow::separator:hover {
+    //     background: red;
+    // }
+
+    // QMainWindow::centralWidget {
+    //     background: yellow;
+    //     width: 10px;
+    //     height: 10px;
+    // }
+
+    // QMainWindow::centralWidget:hover {
+    //     background: red;
+    // }
+
+    // QFrame {
+    //     border: 2px solid green;
+    //     border-radius: 4px;
+    //     padding: 2px;
+    //     background-color: red;
+    //     color: blue;
+    // }
+
+
+    //setStyleSheet(style);
 }
 
 MainWindow3::~MainWindow3()
